b7ea7619c02192cf39a193304635057316a5db1d
ziebamikolaj <ziebamikolaj03@gmail.com>
2024-05-14 15:46:07 +0200

    chore: Add @tanstack/react-query package and update dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "./components/ui/input";
 import { ModeToggle } from "./components/ui/mode-toggle";
 import { z } from "zod";
-import { useEffect, useMemo } from "react";
-import React from "react";
+import { useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const resSchema = z.object({
   pln: z.number(),
@@ -19,7 +13,7 @@ const resSchema = z.object({
 type Currency = z.infer<typeof resSchema>;
 
 const getCurrency = async () => {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}`, {
+  const res = await fetch(`http://localhost:8000`, {
     headers: {
       "Content-Type": "application/json",
     },
@@ -30,24 +24,19 @@ const getCurrency = async () => {
 };
 
 function App() {
-  const [currency, setCurrency] = React.useState<Currency | null>(null);
-  const [error, setError] = React.useState<string | null>(null);
+  const {
+    data: currency,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ["currency"],
+    queryFn: getCurrency,
+    refetchInterval: 1000,
+  });
 
-  useEffect(() => {
-    getCurrency()
-      .then((currency: Currency) => {
-        setCurrency(currency);
-      })
-      .catch(() => {
-        setError(
-          "Failed to fetch currency data. Please check your internet connection."
-        );
-      });
-  }, []);
-
-  const [inputCurrency, setInputCurrency] = React.useState<string>("pln");
-  const [outputCurrency, setOutputCurrency] = React.useState<string>("usd");
-  const [inputValue, setInputValue] = React.useState<number>(0);
+  const [inputCurrency, setInputCurrency] = useState<string>("pln");
+  const [outputCurrency, setOutputCurrency] = useState<string>("usd");
+  const [inputValue, setInputValue] = useState<number>(0);
 
   const converted = useMemo(() => {
     let tempPLN = 0;
@@ -61,30 +50,18 @@ function App() {
     }
     for (const key in currency) {
       if (key === outputCurrency) {
-        return (
-          Math.round(
-            ((inputValue * tempPLN) / currency[key as keyof Currency]) * 100
-          ) / 100
-        );
+        return Math.round(((inputValue * tempPLN) / currency[key as keyof Currency]) * 100) / 100;
       }
     }
   }, [inputCurrency, outputCurrency, inputValue, currency]);
 
   // If there is no connection to the api then display error message
-  if (error) {
-    return (
-      <div className="text-red-500 bg-white p-4 rounded-md shadow-md">
-        {error}
-      </div>
-    );
+  if (isError) {
+    return <div className="text-red-500 bg-white p-4 rounded-md shadow-md">{error.message}</div>;
   }
 
   if (!currency) {
-    return (
-      <div className="flex items-center justify-center h-screen text-2xl text-blue-500">
-        Loading...
-      </div>
-    );
+    return <div className="flex items-center justify-center h-screen text-2xl text-blue-500">Loading...</div>;
   }
 
   return (
@@ -95,12 +72,7 @@ function App() {
       <div className="grid bg-stone-200 dark:bg-[#030712] place-items-center h-screen">
         <div className="grid place-items-center gap-6">
           <div className="flex">
-            <Input
-              className="bg-stone-100 dark:bg-[#030712]"
-              onChange={(event) =>
-                setInputValue(parseFloat(event.target.value))
-              }
-            ></Input>
+            <Input className="bg-stone-100 dark:bg-[#030712]" onChange={(event) => setInputValue(parseFloat(event.target.value))}></Input>
             <Select onValueChange={setInputCurrency}>
               <SelectTrigger className="w-[10rem]">
                 <SelectValue placeholder="PLN" />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
